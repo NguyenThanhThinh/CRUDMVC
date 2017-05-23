3b4be061d0b05124cb3427b37579eaf14152be06
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-23 13:09:28 +0700

    add EF

---
+++ CRUDMVC/CRUDMVC/Migrations/201705230608308_add.cs
@@ -0,0 +1,27 @@
+namespace CRUDMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Category",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 100),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Category");
+        }
+    }
+}
---
+++ CRUDMVC/CRUDMVC/Models/PetCrudMvcDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDMVC.Models.Entities;
+using System.Data.Entity;
+
+namespace CRUDMVC.Models
+{
+    public class PetCrudMvcDbContext:DbContext
+    {
+        public PetCrudMvcDbContext() : base("PetCrudMvcDb")
+        {
+
+        }
+        public DbSet<Category> Categorys { get; set; }
+
+    }
+}
