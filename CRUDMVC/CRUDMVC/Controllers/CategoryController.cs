3db77e71a66e200baf310a916a397b8897b9c3a6
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-23 13:22:17 +0700

    add view index

---
+++ CRUDMVC/CRUDMVC/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using CRUDMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly PetCrudMvcDbContext db = null;
+        public CategoryController()
+        {
+            db = new PetCrudMvcDbContext();
+        }
+        public ActionResult Index()
+        {
+            var getAll = db.Categorys.ToList();
+            return View(getAll);
+        }
+    }
+}
