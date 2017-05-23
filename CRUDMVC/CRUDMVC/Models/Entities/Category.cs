33ca6673d4cd24c266df5dc82bf143f15f8241c4
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-23 13:02:43 +0700

    add entities

---
+++ CRUDMVC/CRUDMVC/Models/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDMVC.Models.Entities
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
